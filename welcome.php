d8e10bd60392bbcd29ed9527f7ab22ed13678a68
iamgauravkr18 <gaurav.mfp80@gamil.com>
2024-04-04 12:11:17 +0530

    uploading files

---
+++ welcome.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+        $name=test_input($_GET);
+        $DOB=test_input($_get);
+        $email=test_input($_GET);
+        $password=test_input($_get);
+        echo ":Hi $name"
+        echo ":your name is $name";
+        echo ":your $DOB";
+        echo ":your $email";
+        echo ":your $password";
+
+
+
+
+
+
+    ?>
+</body>
+</html>
